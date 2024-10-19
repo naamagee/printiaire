45add6bd3d2ac16c08c31c46d188bacc28561302
junaama <0naama0@gmail.com>
2024-10-19 16:50:58 -0400

    finish landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,28 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Instrument_Sans, Instrument_Serif, Parisienne } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const instrumentSans = Instrument_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: "--font-instr-sans",
 
+})
+const instrumentSerif = Instrument_Serif({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400'],
+  variable: "--font-instr-serif",
+
+})
+const parisienne = Parisienne({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400'],
+  variable: "--font-parisienne",
+})
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -26,7 +47,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} ${instrumentSans.className} ${parisienne.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,99 +2,51 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col justify-center items-center  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[#8C8274]">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <section>
+        <div className="relative flex justify-center font-[family-name:var(--font-instr-serif)] text-[#5B410E]">
+          <Image src="/gradient2.png" alt="Next.js logo"
+            width={786}
+            height={743} />
+          <div className="absolute top-1/4 max-w-[300px] text-center text-5xl ">CURATED PRINTS
+            <span className="font-[family-name:var(--font-parisienne)]"> only</span> FOR YOU</div>
+          <div className="font-[family-name:var(--font-instr-serif)] absolute bottom-1/4 text-center text-6xl">
+            PRINTIAIRE.
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      </section>
+      <section className="bg-[#EBE6D2] flex flex-col p-8 text-center w-full text-3xl">
+        <div>connect</div>
+        <div className="font-[family-name:var(--font-parisienne)]">connect your accounts and receive personalized publications</div>
+        <div className="text-left flex items-start text-2xl">
+          <p>pinterest</p>
+          <div className="border-dashed border-[#5B410E] border-2 border-t-0 border-l-0 border-r-0 w-full pt-5"></div>
+        </div>
+        <div className="text-left flex items-start text-2xl"> <p>substack</p>
+        <div className="border-dashed border-[#5B410E] border-2 border-t-0 border-l-0 border-r-0 w-full pt-5"></div></div>
+
+      </section>
+      <section className="font-[family-name:var(--font-instr-serif)] text-3xl text-white text-center ">
+        <div>purchase</div>
+        <div className="font-[family-name:var(--font-parisienne)] pb-8">connect your accounts and receive personalized publications</div>
+        <Image src="/mockup.png" alt="magazine preview" width="1000" height="750" />
+      </section>
+      <footer className="row-start-3 flex gap-6 flex-col items-center justify-center">
+
+        <Image
+          aria-hidden
+          src="/print.png"
+          alt="File icon"
+          width={240}
+          height={240}
+          className="mix-blend-color-burn"
+        />
+        <p className="-mt-16 text-3xl font-[family-name:var(--font-instr-serif)]">PRINTIAIRE</p>
+
+        hello@printiaire.com
+
       </footer>
     </div>
   );
